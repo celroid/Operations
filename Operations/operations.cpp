f31bb427917f2330da2eed41709badb985345c70
Hyu <liliumhyu@gmail.com>
2017-10-29 14:43:57 +0200

    Add project files.

---
+++ Operations/operations.cpp
@@ -0,0 +1,23 @@
+// simple program to exercise operators
+
+//v1.0
+
+#include "../../std_lib_facilities.h"
+
+int main()
+
+{
+	cout << "Please enter a floating-point value: ";
+	double n;
+	cin >> n;
+	cout << "n == " << n
+		<< "\nn+1 == " << n + 1
+		<< "\nthree times n == " << 3 * n
+		<< "\ntwice n == " << n + n
+		<< "\nn squared == " << n*n
+		<< "\nhalf of n == " << n / 2
+		<< "\nsquare root of n == " << sqrt(n)
+		<< '\n'; //another name for newline ("end of line") in output
+	keep_window_open(); //enter a character to exit
+
+}
