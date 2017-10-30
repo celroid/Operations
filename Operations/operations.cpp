4f4f311a6b8e7effc3ec40ba3c4dfe177da909bd
Hyu <liliumhyu@gmail.com>
2017-10-30 20:22:06 +0200

    version 2

--- Operations/operations.cpp
+++ Operations/operations.cpp
@@ -1,15 +1,19 @@
 // simple program to exercise operators
 
-//v1.0
+//v2.0
 
 #include "../../std_lib_facilities.h"
 
 int main()
 
 {
-	cout << "Please enter a floating-point value: ";
-	double n;
+
+	int n;
+	int a;
+	cout << "Please enter an int value: \n";
 	cin >> n;
+	cout << "Please enter an int value: \n";
+	cin >> a;
 	cout << "n == " << n
 		<< "\nn+1 == " << n + 1
 		<< "\nthree times n == " << 3 * n
@@ -17,7 +21,9 @@ int main()
 		<< "\nn squared == " << n*n
 		<< "\nhalf of n == " << n / 2
 		<< "\nsquare root of n == " << sqrt(n)
+		<< "((n/b) *a) + (n % a) == " << ((n / a) *a) + (n % a)
 		<< '\n'; //another name for newline ("end of line") in output
+
 	keep_window_open(); //enter a character to exit
 
 }
